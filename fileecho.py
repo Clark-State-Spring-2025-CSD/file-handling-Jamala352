317c66a4905a60ef9ef6eb1341010ddca1f79154
Jamala352 <ashlej2513@clarkstate.edu>
2025-04-06 23:01:41 -0400

    Update fileecho.py

--- fileecho.py
+++ fileecho.py
@@ -1,3 +1,9 @@
 #Read in the echo.txt file and display it in the terminal using print()
 #This is a guided practice. Either follow the video or your instructor in class.
 
+# Open the file in read mode
+with open('echo.txt', 'r') as file:
+    # Read the contents of the file
+    content = file.read()
+    # Print the content to the terminal
+    print(content)
